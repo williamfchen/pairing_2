b5df1eed729ff3c979df5d077c089562970be4a5
Will Chen <wfchen1990@gmail.com>
2023-06-01 13:52:44 -0600

    Add and output a modified array

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["John", "Matthew", "Rita", "Lisa", "Laura"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["Jamaica", "Israel", "Russa", "Lisbon", "Lithuania"]
+places.shift
+puts places
